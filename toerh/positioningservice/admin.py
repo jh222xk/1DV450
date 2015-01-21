3013adcd6f5a86498b5b67a9c8a7da582f473b24
Jesper Håkansson <jh222xk@student.lnu.se>
2015-01-21 22:33:25 +0100

    Added models to admin

--- toerh/positioningservice/admin.py
+++ toerh/positioningservice/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Position, Event, Tag
 
-# Register your models here.
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'latitude', 'longitude')
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('position', 'user')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Position, PositionAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Tag, TagAdmin)
