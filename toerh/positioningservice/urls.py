ca5d825a2793cff091112268bf8682c4d75cab4e
Jesper Håkansson <jh222xk@student.lnu.se>
2015-01-26 23:05:08 +0100

    [Feature] Added oauth for the API

--- toerh/positioningservice/urls.py
+++ toerh/positioningservice/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, url
 from .views import PositionView
 
 urlpatterns = patterns('positioningservice.views',
-                       url(r'^$', PositionView.as_view(), name='position-list')
+                       url(r'^$', PositionView.as_view(), name='position-list'),
+                       url(r'^(?P<pk>[-_\w]+)$', PositionView.as_view(), name='position-detail'),
                        )
