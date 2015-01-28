0b476da95a302186faa8c06d15f92830fbb6de42
Jesper Håkansson <jh222xk@student.lnu.se>
2015-01-28 18:26:12 +0100

    [API] Added CRUD for positions

--- toerh/positioningservice/urls.py
+++ toerh/positioningservice/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, url
-from .views import PositionView, ClientCreate
+from .views import PositionListView, PositionDetailView
 
 urlpatterns = patterns('positioningservice.views',
-                       url(r'^$', PositionView.as_view(), name='list'),
-                       # url(r'^(?P<pk>[-_\w]+)$', PositionView.as_view(), name='position-detail'),
+                       url(r'^$', PositionListView.as_view(), name='list'),
+                       url(r'^(?P<pk>[-_\w]+)$', PositionDetailView.as_view(), name='position-detail'),
                        )
--- toerh/positioningservice/views.py
+++ toerh/positioningservice/views.py
@@ -1,26 +1,24 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from rest_framework import generics
-from oauth2_provider.models import Application
 
 from .models import Position
 from .serializers import PositionSerializer
 
 
-class PositionView(generics.ListAPIView):
+class PositionListView(generics.ListAPIView):
 
     """
-    Returns a list of all authors.
+    Returns a list of all positions.
     """
     queryset = Position.objects.all()
     serializer_class = PositionSerializer
 
 
-class ClientCreate(CreateView):
-    model = Application
-    fields = ['name']
+class PositionDetailView(generics.RetrieveUpdateDestroyAPIView):
 
-
-# class ClientUpdate(UpdateView):
-#     model = Client
-#     fields = ['name']
+    """
+    Returns a single position.
+    """
+    queryset = Position.objects.all()
+    serializer_class = PositionSerializer
