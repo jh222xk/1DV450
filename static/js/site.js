240acdaadfbf86daa6f86e60ad7eb0d817ebd249
Jesper Håkansson <jh222xk@student.lnu.se>
2015-02-10 20:36:41 +0100

    [Style] Added info messages

--- static/js/site.js
+++ static/js/site.js
@@ -1,4 +1,7 @@
 $(function() {
   $('.ui.dropdown').dropdown();
   $('input').popup({on: 'focus'});
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').fadeOut('slow');
+  });
 })
