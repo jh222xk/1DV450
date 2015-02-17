4609d90805fb577cc1238f94bfb9c023617fc491
Jesper Håkansson <jh222xk@student.lnu.se>
2015-02-17 12:04:21 +0100

    [Tests] Added more tests for tokens

--- tokens/tests/test_views.py
+++ tokens/tests/test_views.py
@@ -15,12 +15,36 @@ class TokenTest(TestCase):
     Test for tokens
     """
 
+    def setUp(self):
+        """
+        Here we set up our test objects for the tests
+        """
+        self.user_obj = User.objects.create_user('test', "asd@asd.se", 'password')
+
+        self.token = Token.objects.create(
+            user=self.user_obj
+        )
+
+    def tearDown(self):
+        self.user_obj.delete()
+        self.token.delete()
+
+    def test_authorized_user_can_view_token(self):
+        # Login user
+        self.client.login(username='test', password='password')
+
+        # Send request to the tokens list url
+        response = self.client.get(reverse('tokens:list'))
+
+        # Check that our token is there.
+        self.assertEqual(response.context['tokens'][0], self.token)
+
     def test_to_view_token_requires_login(self):
         """
         Test method for checking that to view token
         requires user to be logged in
         """
-        # Send request to the positions API
+        # Send request to the tokens list url
         response = self.client.get(reverse('tokens:list'))
 
         # Check that we cannot access that url (redirection to the login url)
@@ -39,26 +63,24 @@ class TokenTest(TestCase):
         self.assertRedirects(response, "%s?next=%s" % (reverse('login'), reverse(
             'tokens:new')), status_code=302, target_status_code=200)
 
-
     def test_to_create_a_new_token_requires_login(self):
         """
         Test method for checking that to create a new token
         requires user to be logged in
         """
-        # Send request to the positions API
+        # Send request to the tokens new url
         response = self.client.get(reverse('tokens:new'))
 
         # Check that we cannot access that url (redirection to the login url)
         self.assertRedirects(response, "%s?next=%s" % (reverse('login'), reverse(
             'tokens:new')), status_code=302, target_status_code=200)
 
-
     def test_to_delete_a_token_requires_login(self):
         """
         Test method for checking that delete a view token
         requires user to be logged in
         """
-        # Send request to the positions API
+        # Send request to the tokens delete url
         response = self.client.get(reverse('tokens:delete', kwargs={'pk': 1}))
 
         # Check that we cannot access that url (redirection to the login url)
