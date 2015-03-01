972fc4f56e9f9ca4a732834a72709a32b33b1cfc
Jesper Håkansson <jh222xk@student.lnu.se>
2015-03-01 12:57:48 +0100

    [Feat] Indexing new coffeehouses upon creation and some minor refactoring

--- tokens/models.py
+++ tokens/models.py
@@ -1,11 +1,15 @@
 import binascii
 import os
+from django.conf import settings
+
 from django.db import models
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class Token(models.Model):
     key = models.CharField(max_length=40, primary_key=True)
-    user = models.ForeignKey('auth.User')
+    user = models.ForeignKey(AUTH_USER_MODEL)
     created = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
