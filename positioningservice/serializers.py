6689659818f42068a181e6a8a858076406ea0ddb
Jesper Håkansson <jh222xk@student.lnu.se>
2015-03-04 21:58:41 +0100

    [Fix] Fixed bug according to #18

--- positioningservice/serializers.py
+++ positioningservice/serializers.py
@@ -2,7 +2,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 
 from rest_framework.fields import CharField
-from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField
+from rest_framework.reverse import reverse
+from rest_framework.serializers import ModelSerializer
 from rest_framework.validators import UniqueValidator
 from rest_framework import serializers
 
@@ -10,19 +11,27 @@ from .models import Position, Tag, Coffee, Review
 
 
 class UserSerializer(ModelSerializer):
-
     """
     Serializing all the Users
     """
-    url = HyperlinkedIdentityField(view_name='api-v1:user-detail')
+    links = serializers.SerializerMethodField()
     email = serializers.EmailField(
         validators=[UniqueValidator(queryset=User.objects.all())]
     )
 
     class Meta:
         model = get_user_model()
-        fields = (User.USERNAME_FIELD, 'email', 'date_joined', 'password', 'last_login', 'is_active', 'url',)
+        fields = (User.USERNAME_FIELD, 'email', 'date_joined', 'password', 'last_login', 'is_active', 'links',)
         write_only_fields = ('password',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('api-v1:user-detail',
+                            kwargs={'pk': obj.pk}, request=request) + '?key={}'.format(request.query_params.get('key')),
+
+
+        }
 
     def create(self, validated_data):
         """
@@ -35,12 +44,10 @@ class UserSerializer(ModelSerializer):
 
 
 class TagSerializer(ModelSerializer):
-
     """
     Serializing all the Tags
     """
-
-    url = HyperlinkedIdentityField(view_name='api-v1:tag-detail')
+    links = serializers.SerializerMethodField()
     user = UserSerializer(many=False, read_only=True)
     name = CharField(
         validators=[UniqueValidator(queryset=Tag.objects.all())]
@@ -48,37 +55,64 @@ class TagSerializer(ModelSerializer):
 
     class Meta:
         model = Tag
-        fields = ('name', 'url', 'user')
+        fields = ('name', 'links', 'user')
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('api-v1:tag-detail',
+                            kwargs={'pk': obj.pk}, request=request) + '?key={}'.format(request.query_params.get('key')),
+
+
+        }
 
     def validate_user(self):
         return self.context['request'].user
 
 
 class PositionSerializer(ModelSerializer):
-
     """
     Serializing all the Positions
     """
 
-    url = HyperlinkedIdentityField(
-        view_name='api-v1:position-detail')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Position
-        fields = ('longitude', 'latitude', 'url', 'created_at',)
+        fields = ('longitude', 'latitude', 'links', 'created_at',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('api-v1:position-detail',
+                            kwargs={'pk': obj.pk}, request=request) + '?key={}'.format(request.query_params.get('key')),
+
+
+        }
 
 
 class ReviewSerializer(ModelSerializer):
     """
     Serializing all the Reviews
     """
-    url = HyperlinkedIdentityField(view_name='api-v1:review-detail')
-    user_url = HyperlinkedIdentityField(view_name='api-v1:user-detail')
-    coffee_url = HyperlinkedIdentityField(view_name='api-v1:coffeehouses-detail')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
-        fields = ('rating', 'description', 'url','coffee', 'coffee_url', 'user_url')
+        fields = ('rating', 'description', 'links',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('api-v1:review-detail',
+                            kwargs={'pk': obj.pk}, request=request) + '?key={}'.format(request.query_params.get('key')),
+            'user': reverse('api-v1:user-detail', kwargs={'pk': obj.user.pk},
+                            request=request) + '?key={}'.format(request.query_params.get('key')),
+            'coffeehouse': reverse('api-v1:coffeehouses-detail', kwargs={'pk': obj.coffee.pk},
+                                   request=request) + '?key={}'.format(request.query_params.get('key')),
+
+
+        }
 
     def validate_user(self):
         return self.context['request'].user
@@ -98,34 +132,42 @@ class CoffeeSerializer(ModelSerializer):
     """
     position = PositionSerializer(many=False, read_only=True)
     reviews = ReviewCoffee(many=True, read_only=True, source='review')
-    url = HyperlinkedIdentityField(view_name='api-v1:coffeehouses-detail')
+    links = serializers.SerializerMethodField()
     tags = TagSerializer(many=True)
 
     class Meta:
         model = Coffee
-        fields = ('name', 'rating', 'url', 'position', 'tags', 'reviews',)
+        fields = ('name', 'rating', 'links', 'description', 'position', 'tags', 'reviews',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('api-v1:coffeehouses-detail',
+                            kwargs={'pk': obj.pk}, request=request) + '?key={}'.format(request.query_params.get('key')),
 
 
-class SimpleCoffeeSerializer(ModelSerializer):
-    """
-    A simple coffeehouse with just the URL, used when nesting
-    """
-    url = HyperlinkedIdentityField(view_name='api-v1:coffeehouses-detail')
-
-    class Meta:
-        model = Coffee
-        fields = ('url',)
+        }
 
 
 class NestedReviewSerializer(ModelSerializer):
     """
     A nested review with just the URL and the coffeehouse, used when nesting
     """
-    coffeehouse = SimpleCoffeeSerializer(source='coffee')
-    url = HyperlinkedIdentityField(view_name='api-v1:review-detail')
+    links = serializers.SerializerMethodField()
 
     class Meta:
         model = Review
-        fields = ('rating', 'description', 'url', 'coffeehouse', )
+        fields = ('rating', 'description', 'links',)
+
+    def get_links(self, obj):
+        request = self.context['request']
+        return {
+            'self': reverse('api-v1:review-detail',
+                            kwargs={'pk': obj.pk}, request=request) + '?key={}'.format(request.query_params.get('key')),
+            'coffeehouse': reverse('api-v1:coffeehouses-detail', kwargs={'pk': obj.coffee.pk},
+                                   request=request) + '?key={}'.format(request.query_params.get('key')),
+
+
+        }
 
 
